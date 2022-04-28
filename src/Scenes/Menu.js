2bc6434ce1130b959d939fe4a350892d1638baf5
Astric215 <davidiwd215@gmail.com>
2022-04-28 15:44:28 -0700

    changed title background

--- src/Scenes/Menu.js
+++ src/Scenes/Menu.js
@@ -4,6 +4,12 @@ class Menu extends Phaser.Scene {
     }
 
     preload() {
+        // BG
+        this.load.image('front', './assets/front.png');
+        this.load.image('bg', './assets/bg.png');
+        this.load.image('next1', './assets/next1.png');
+        this.load.image('next2', './assets/next2.png');
+        this.load.image('next3', './assets/next3.png');
       }
 
     create() {
@@ -23,6 +29,12 @@ class Menu extends Phaser.Scene {
             },
             fixedWidth: 0
         }
+        //background creation
+        this.bgr = this.add.tileSprite(0, 0, 1000, 1000, 'bg').setOrigin(0, 0);
+        this.next1 = this.add.tileSprite(0, 0, 1000, 1000, 'next3').setOrigin(0, 0);
+        this.next2 = this.add.tileSprite(0, 0, 1000, 1000, 'next2').setOrigin(0, 0);
+        this.next3 = this.add.tileSprite(0, 0, 1000, 1000, 'next1').setOrigin(0, 0);
+        this.front = this.add.tileSprite(0, 0, 1000, 1000, 'front').setOrigin(0, 0);
 
         //write
         this.add.text(game.config.width/2, game.config.height/2 - 80, '(:)', menuConfig).setOrigin(0.5);
@@ -32,6 +44,7 @@ class Menu extends Phaser.Scene {
         menuConfig.backgroundColor = '#FF0000';
         menuConfig.color = '#000';
 
+       
     }
     update() {
         if (Phaser.Input.Keyboard.JustDown(keyA)) {
